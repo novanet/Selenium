dc258ba876eba9458194e17fd9854ffb7e0ae0c4
hbrotan <hbrotan@gmail.com>
2017-09-12 14:12:01 +0200

    Basic example

---
+++ 1. Basic example/Novanet.Tests.UI/Infrastructure/WebDriver.cs
@@ -0,0 +1,44 @@
+﻿using System.IO;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using TechTalk.SpecFlow;
+
+namespace Novanet.Test.UI.Infrastructure
+{
+    [Binding]
+    public static class WebDriver
+    {
+        private static IWebDriver _instance;
+
+        public static IWebDriver Current => _instance ?? (_instance = GetChromeDriver());
+
+        private static IWebDriver GetChromeDriver()
+        {
+            Directory.SetCurrentDirectory(TestContext.CurrentContext.TestDirectory);
+
+            var service = ChromeDriverService.CreateDefaultService(Path.GetFullPath(@"Infrastructure\ChromeDriver\"));
+
+            var options = new ChromeOptions();
+            options.AddArguments("--start-maximized");
+
+            return new ChromeDriver(service, options);
+        }
+
+        [AfterTestRun]
+        public static void Kill()
+        {
+            if (_instance == null)
+                return;
+
+            try
+            {
+                _instance.Manage().Cookies.DeleteAllCookies();
+            }
+            finally
+            {
+                _instance.Quit();
+            }
+        }
+    }
+}
